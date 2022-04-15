6d395f947558e26155c6b4633ca92ba0349fe5f6
MohamadAwaly <awaly.mohamad@gmail.com>
2022-04-15 10:24:12 +0200

    create class InvoiceForm

--- src/main/java/com/mycompany/invoise/invoiceweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiceweb/controller/InvoiceControllerWeb.java
@@ -3,14 +3,18 @@ package com.mycompany.invoise.invoiceweb.controller;
 import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
 import com.mycompany.invoise.core.entity.Invoice;
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import com.mycompany.invoise.invoiceweb.form.InvoiceForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @Controller
-@RequestMapping( "/invoice" )
-public class InvoiceControllerWeb implements InvoiceControllerInterface {
+@RequestMapping("/invoice")
+public class InvoiceControllerWeb {
     @Autowired
     private InvoiceServiceInterface invoiceService;
 
@@ -18,32 +22,39 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
         return invoiceService;
     }
 
-    public void setInvoiceService( InvoiceServiceInterface invoiceService ) {
+    public void setInvoiceService(InvoiceServiceInterface invoiceService) {
         this.invoiceService = invoiceService;
     }
 
-    @PostMapping( "" )
-    public String createInvoice( @ModelAttribute Invoice invoice ) {
-        invoiceService.createInvoice( invoice );
+    @PostMapping("")
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results) {
+        if(results.hasErrors()){
+            return "invoice-create-form";
+        }
+
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(invoiceForm.getCustomerName());
+        invoice.setOrderNumber(invoiceForm.getCustomerName());
+        invoiceService.createInvoice(invoice);
         return "invoice-created";
     }
 
-    @GetMapping( "/home" )
-    public String displayHome( Model model ) {
-        System.out.println( "La méthode display home a était invoqué" );
-        model.addAttribute( "invoices", invoiceService.getInvoiceList() );
+    @GetMapping("/home")
+    public String displayHome(Model model) {
+        System.out.println("La méthode display home a était invoqué");
+        model.addAttribute("invoices", invoiceService.getInvoiceList());
         return "invoice-home";
     }
 
-    @GetMapping( "/{id}" )
-    public String displayInvoice( @PathVariable( "id" ) String number, Model model ) {
-        System.out.println( "La méthode display invoice a était invoqué" );
-        model.addAttribute( "invoice", invoiceService.getInvoiceByNumber( number ) );
+    @GetMapping("/{id}")
+    public String displayInvoice(@PathVariable("id") String number, Model model) {
+        System.out.println("La méthode display invoice a était invoqué");
+        model.addAttribute("invoice", invoiceService.getInvoiceByNumber(number));
         return "invoice-details";
     }
 
-    @GetMapping( "/create-form" )
-    public String displayInvoiceCreateForm( @ModelAttribute Invoice invoice ) {
+    @GetMapping("/create-form")
+    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoice) {
         return "invoice-create-form";
     }
 }
